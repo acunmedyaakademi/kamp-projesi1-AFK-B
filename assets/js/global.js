4b56af840ef7a43df1214399b53212123a802879
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-02-12 16:50:33 +0300

    product-detail ve cart javascript tamamlandı

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,4 +1,14 @@
 export let data = [];
+export let myCart = [];
+
+export function saveCart() {
+  localStorage.myCart = JSON.stringify(myCart);
+}
+
+export const formatPrice = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
 async function getData() {
   const response = await fetch("../../data.json").then((x) => x.json());
@@ -10,6 +20,12 @@ if (localStorage.data) {
   data = JSON.parse(localStorage.data);
 } else {
   data = await getData();
+}
+
+if (localStorage.myCart) {
+  myCart = JSON.parse(localStorage.myCart);
+} else {
+  myCart = [];
 }
 
 const hamburgerMenu = document.querySelector(".hamburgerMenuInput");
@@ -36,4 +52,76 @@ cartInput.addEventListener("change", (e) => {
     cartDialog.open = false;
     document.body.style = "overflow: auto";
   }
+});
+
+const cartNum = document.querySelector(".cart-header h2");
+const cartItemList = document.querySelector(".cart-item-list");
+
+export function updateCart() {
+  cartNum.textContent = `CART (${myCart.length})`;
+  cartItemList.innerHTML = myCart
+    .map(
+      (item) => `
+    <div class="cart-item">
+      <img src="${item.image.mobile}" alt="${item.name}" />
+      <div class="cart-item-metadata">
+        <h3>${item.name}</h3>
+        <p>$ ${item.price}</p>
+      </div>
+      <div class="cart-item-count">
+        <button type="button" class="cart-item-countDec" data-id="${item.id}">-</button>
+        <span>${item.count}</span>
+        <button type="button" class="cart-item-countInc" data-id="${item.id}">+</button>
+      </div>
+    </div>
+  `
+    )
+    .join("");
+
+  const cartItemCountDec = document.querySelectorAll(".cart-item-countDec");
+  const cartItemCountInc = document.querySelectorAll(".cart-item-countInc");
+
+  cartItemCountDec.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const id = e.target.dataset.id;
+      const item = myCart.find((x) => x.id == id);
+
+      if (item.count > 1) {
+        item.count--;
+      } else {
+        myCart = myCart.filter((item) => item.id != id);
+      }
+      saveCart();
+      updateCart();
+    });
+  });
+
+  cartItemCountInc.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const id = e.target.dataset.id;
+      const item = myCart.find((x) => x.id == id);
+      item.count++;
+      saveCart();
+      updateCart();
+    });
+  });
+
+  const cartTotal = document.querySelector(".cart-total");
+
+  let total = 0;
+  myCart.map((item) => {
+    total += item.price * item.count;
+  });
+
+  cartTotal.textContent = `$ ${formatPrice.format(total).substring(1)}`;
+}
+
+updateCart();
+
+const removeAllCart = document.querySelector(".cart-header button");
+
+removeAllCart.addEventListener("click", () => {
+  myCart = [];
+  saveCart();
+  updateCart();
 });
--- assets/js/product-detail.js
+++ assets/js/product-detail.js
@@ -1,4 +1,83 @@
-import { data } from "./global.js";
+import { data, formatPrice, myCart, saveCart, updateCart } from "./global.js";
 
-const id = location.hash.substring(1) || 1;
-const currentData = data.find(x => x.id == id);
+const productDetailText = document.querySelector(".product-detail-text");
+const productDetailItemImg = document.querySelector(".product-detail-item img");
+const productFeaturesText = document.querySelector(".product-features-header p");
+const productBoxItemsContent = document.querySelector(".product-box-items-content");
+const productDetailImages = document.querySelector(".product-detail-images");
+const productAlsoLike = document.querySelector(".product-also-like");
+
+let id = location.hash.substring(1) || 1;
+let currentData = data.find((x) => x.id == id);
+
+function getHash() {
+  id = location.hash.substring(1) || 1;
+  currentData = data.find((x) => x.id == id);
+  updateContent(currentData);
+  window.scrollTo(0, 0);
+}
+
+window.addEventListener("hashchange", getHash);
+
+function updateContent(selectedItem) {
+  let count = 1;
+
+  productDetailItemImg.src = `${selectedItem.image.desktop}`;
+
+  productDetailText.innerHTML = `
+  ${selectedItem.isNew ? "<h4>NEW PRODUCT</h4>" : ""}
+  <h3>${selectedItem.name}</h3>
+  <p>${selectedItem.description}</p>
+  <h3>$ ${formatPrice.format(selectedItem.price).substring(1)}</h3>
+  <div class="product-detail-count-content">
+    <div class="product-detail-count">
+      <button class="product-detail-countDec">-</button>
+      <span>${count}</span>
+      <button class="product-detail-countInc">+</button>
+    </div>
+    <button class="add-cartBtn">ADD TO CART</button>
+  </div>
+  `;
+
+  document.querySelector(".product-detail-countInc").addEventListener("click", () => {
+    count++;
+    document.querySelector(".product-detail-count span").innerText = count;
+  });
+
+  document.querySelector(".product-detail-countDec").addEventListener("click", () => {
+    if (count > 1) {
+      count--;
+      document.querySelector(".product-detail-count span").innerText = count;
+    }
+  });
+
+  document.querySelector(".add-cartBtn").addEventListener("click", () => {
+    myCart.push({ ...selectedItem, count });
+    saveCart();
+    updateCart();
+  });
+
+  productFeaturesText.innerHTML = selectedItem.features;
+
+  productBoxItemsContent.innerHTML = selectedItem.includes.map((x) => `
+    <div class="product-box-item">
+      <span class="product-box-item-count">${x.quantity}x</span>
+      <span class="product-box-item-name">${x.item}</span>
+    </div>
+  `).join("");
+
+  productDetailImages.innerHTML = selectedItem.gallery.map((x) => `
+    <img src="${x.desktop}" alt="${selectedItem.name}">
+  `).join("");
+
+  productAlsoLike.innerHTML = "<h2>YOU MAY ALSO LIKE</h2>";
+  productAlsoLike.innerHTML += selectedItem.others.map((x) => `
+    <div class="product-also-like-item">
+      <img src="${x.image.desktop}" alt="${x.name}" />
+      <h3>${x.name}</h3>
+      <a href="#${data.find(y => y.slug === x.slug).id}">SEE PRODUCT</a>
+    </div>
+  `).join("");
+}
+
+updateContent(currentData);
